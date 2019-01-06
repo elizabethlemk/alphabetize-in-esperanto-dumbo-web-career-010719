c55a96439a218834d77275d874848d6329aa2998
ELIZABETH LE <elizabethlemk@yahoo.com>
2019-01-06 01:05:16 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
 ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |word|
-    word.split(' ').map do |letter|
+    word.split('').map do |letter|
       ALPHABET.index(letter)
     end
   end
