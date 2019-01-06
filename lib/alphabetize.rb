d6ff22173633be39e5786f8c1400e7905fd6d198
ELIZABETH LE <elizabethlemk@yahoo.com>
2019-01-06 01:09:09 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,4 @@
-ALPHABET = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |word|
     word.split('').map do |letter|
